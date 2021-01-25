ef35b5b60449aaa86924f157b30b5d45f9170d19
pedrocasimiro <pedro_sc90@upskill.pt>
2021-01-25 15:41:56 +0000

    "Classe Cliente Modelo"

---
+++ API/ParqueAPI/Models/Cliente.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParqueAPI.Models
+{
+    public class Cliente
+    {
+        public long ClienteID { get; set; }
+        public string NomeCliente { get; set; }
+        public string EmailCliente { get; set; }
+        public int NifCliente { get; set; }
+        public string MetodoPagamento { get; set; }
+        public int Credito { get; set; }
+
+    }
+}
