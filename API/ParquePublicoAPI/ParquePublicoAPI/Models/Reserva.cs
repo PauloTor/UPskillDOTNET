2cf01881ff9753700c2c2ad88f51290efdf38d5c
Leandro-Caetano <leandromccaetano@upskill.pt>
2021-01-26 17:17:20 +0000

    models

--- API/ParquePublicoAPI/ParquePublicoAPI/Models/Reserva.cs
+++ API/ParquePublicoAPI/ParquePublicoAPI/Models/Reserva.cs
@@ -2,10 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ParquePublicoAPI.Models
 {
     public class Reserva
     {
+        public long ReservaID { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime DataReserva { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime DataInicio { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime DataFim { get; set; }
+
+        [ForeignKey("LugarID")]
+        public long LugarID { get; set; }
+        public Lugar Lugar { get; set; }
     }
 }
