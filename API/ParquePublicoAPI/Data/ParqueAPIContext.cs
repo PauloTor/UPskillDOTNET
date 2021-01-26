4794c218b1b23b331202019918873e74ebc1c56b
VicDuarte <duarte92rafc@upskill.pt>
2021-01-26 16:31:20 +0000

    Criação API Parque Público

---
+++ API/ParquePublicoAPI/Data/ParqueAPIContext.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ParqueAPI.Models;
+using ParqueAPI.Data;
+
+namespace ParqueAPI.Data
+{
+    public class ParqueAPIContext : DbContext
+    {
+        public ParqueAPIContext (DbContextOptions<ParqueAPIContext> options)
+            : base(options)
+        {
+            
+        }
+
+         
+
+        public DbSet<ParqueAPI.Models.Rua> Morada { get; set; }
+
+        public DbSet<ParqueAPI.Models.Parque> Parque { get; set; }
+
+        public DbSet<ParqueAPI.Models.Cliente> Cliente { get; set; }
+
+        public DbSet<ParqueAPI.Models.Fatura> Fatura { get; set; }
+
+        public DbSet<ParqueAPI.Models.Reserva> Reserva { get; set; }
+
+        public DbSet<ParqueAPI.Models.Lugar> Lugar { get; set; }
+
+        public DbSet<ParqueAPI.Models.SubAluguer> SubAluguer { get; set; }
+    }
+}
