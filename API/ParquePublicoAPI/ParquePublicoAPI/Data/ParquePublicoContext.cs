6e7e5252569c0fa1bb3ef445bc22903fa025beab
VicDuarte <duarte92rafc@upskill.pt>
2021-01-26 17:08:08 +0000

    update API Parque Publico

---
+++ API/ParquePublicoAPI/ParquePublicoAPI/Data/ParquePublicoContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ParquePublicoAPI.Models;
+using ParquePublicoAPI.Data;
+
+namespace ParquePublicoAPI.Data
+{
+    public class ParquePublicoContext : DbContext
+    {
+        public ParquePublicoContext (DbContextOptions<ParquePublicoContext> options)
+            : base(options)
+        {
+
+        }
+
+
+    }
+}
