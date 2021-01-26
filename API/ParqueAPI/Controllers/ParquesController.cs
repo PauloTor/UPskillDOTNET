d6d9e23805fea12908014e2ebff61ab530f7147b
Leandro-Caetano <leandromccaetano@upskill.pt>
2021-01-26 12:41:21 +0000

    parque contr

--- API/ParqueAPI/Controllers/ParquesController.cs
+++ API/ParqueAPI/Controllers/ParquesController.cs
@@ -25,7 +25,7 @@ namespace ParqueAPI.Controllers
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Parque>>> GetParques()
         {
-            return await _context.Parque.Include(p => p.Morada).Include(p => p.MoradaID).ToListAsync();
+            return await _context.Parque.Include(p => p.Morada).ToListAsync();
         }
 
         // GET: api/Parques/5
