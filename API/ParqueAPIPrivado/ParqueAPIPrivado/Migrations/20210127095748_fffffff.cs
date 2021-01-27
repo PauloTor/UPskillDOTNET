3b911f04e3da3ca8e1fa112f40e0f13aaa7b15fe
Paulo-Torcato <paulotorcato@upskill.pt>
2021-01-27 10:06:14 +0000

    migration

---
+++ API/ParqueAPIPrivado/ParqueAPIPrivado/Migrations/20210127095748_fffffff.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ParqueAPIPrivado.Migrations
+{
+    public partial class fffffff : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
