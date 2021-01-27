7a8f69f23ebc46fcb114d1012148561f0f6a495f
pedrocasimiro <pedro_sc90@upskill.pt>
2021-01-27 11:03:14 +0000

    "DBAtualizaçao"

--- API/ParquePublicoAPI/ParquePublicoAPI/Data/DbInitializer.cs
+++ API/ParquePublicoAPI/ParquePublicoAPI/Data/DbInitializer.cs
@@ -21,15 +21,15 @@ namespace ParquePublicoAPI.Data
             var lugar = new Lugar[]
 
                 {
-                    new Lugar { LugarID = 1, PrecoLugar = 2.00f, RuaID= 1 },
-                    new Lugar { LugarID = 2, PrecoLugar = 3.10f, RuaID= 2 },
-                    new Lugar { LugarID = 3, PrecoLugar = 2.00f, RuaID= 3 },
-                    new Lugar { LugarID = 4, PrecoLugar = 1.00f, RuaID= 4 },
-                    new Lugar { LugarID = 5, PrecoLugar = 2.00f, RuaID= 8 },
-                    new Lugar { LugarID = 6, PrecoLugar = 2.00f, RuaID= 5 },
-                    new Lugar { LugarID = 7, PrecoLugar = 1.50f, RuaID= 6 },
-                    new Lugar { LugarID = 8, PrecoLugar = 4.00f, RuaID= 9 },
-                    new Lugar { LugarID = 9, PrecoLugar = 2.50f, RuaID= 7 }
+                    new Lugar { PrecoLugar = 2.00f, RuaID= 1 },
+                    new Lugar { PrecoLugar = 3.10f, RuaID= 2 },
+                    new Lugar { PrecoLugar = 2.00f, RuaID= 3 },
+                    new Lugar { PrecoLugar = 1.00f, RuaID= 4 },
+                    new Lugar { PrecoLugar = 2.00f, RuaID= 8 },
+                    new Lugar { PrecoLugar = 2.00f, RuaID= 5 },
+                    new Lugar { PrecoLugar = 1.50f, RuaID= 6 },
+                    new Lugar { PrecoLugar = 4.00f, RuaID= 9 },
+                    new Lugar { PrecoLugar = 2.50f, RuaID= 7 }
                 };
 
             foreach (Lugar s in lugar)
@@ -61,10 +61,10 @@ namespace ParquePublicoAPI.Data
             var reserva = new Reserva[]
 
                {
-                   new Reserva { ReservaID = 1, DataReserva = DateTime.Parse("2020-1-26 15:00:00"), DataInicio = DateTime.Parse("2020-02-12 16:00:00"), DataFim = DateTime.Parse("2020-02-12 17:00:00"), LugarID = 1},
-                   new Reserva { ReservaID = 2, DataReserva = DateTime.Parse("2020-1-26 16:00:00"), DataInicio = DateTime.Parse("2020-02-12 15:00:00"), DataFim = DateTime.Parse("2020-02-12 18:00:00"), LugarID = 2},
-                   new Reserva { ReservaID = 3, DataReserva = DateTime.Parse("2020-1-27 09:00:00"), DataInicio = DateTime.Parse("2020-02-12 15:00:00"), DataFim = DateTime.Parse("2020-02-12 18:00:00"), LugarID = 1},
-                   new Reserva { ReservaID = 4, DataReserva = DateTime.Parse("2020-1-27 10:00:00"), DataInicio = DateTime.Parse("2020-02-13 09:00:00"), DataFim = DateTime.Parse("2020-02-13 21:00:00"), LugarID = 1}
+                   new Reserva { DataReserva = DateTime.Parse("2020-1-26 15:00:00"), DataInicio = DateTime.Parse("2020-02-12 16:00:00"), DataFim = DateTime.Parse("2020-02-12 17:00:00"), LugarID = 1},
+                   new Reserva { DataReserva = DateTime.Parse("2020-1-26 16:00:00"), DataInicio = DateTime.Parse("2020-02-12 15:00:00"), DataFim = DateTime.Parse("2020-02-12 18:00:00"), LugarID = 2},
+                   new Reserva { DataReserva = DateTime.Parse("2020-1-27 09:00:00"), DataInicio = DateTime.Parse("2020-02-12 15:00:00"), DataFim = DateTime.Parse("2020-02-12 18:00:00"), LugarID = 1},
+                   new Reserva { DataReserva = DateTime.Parse("2020-1-27 10:00:00"), DataInicio = DateTime.Parse("2020-02-13 09:00:00"), DataFim = DateTime.Parse("2020-02-13 21:00:00"), LugarID = 1}
                };
 
             foreach (Reserva r in reserva)
