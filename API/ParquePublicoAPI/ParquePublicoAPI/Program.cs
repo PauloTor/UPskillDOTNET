3ef0efedb3b77173bc34b7fe9d5b91996ef70cbb
pedrocasimiro <pedro_sc90@upskill.pt>
2021-01-28 16:50:52 +0000

    "AuthenticationInPublic"

--- API/ParquePublicoAPI/ParquePublicoAPI/Program.cs
+++ API/ParquePublicoAPI/ParquePublicoAPI/Program.cs
@@ -44,7 +44,8 @@ namespace ParquePublicoAPI
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseStartup<Startup>()
+                        .UseUrls("http://localhost:4000");
                 });
     }
 }
