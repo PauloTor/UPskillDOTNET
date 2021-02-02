be5133127af596195b71db7008286aff01fc8a51
Paulo-Torcato <paulotorcato@upskill.pt>
2021-02-02 11:02:42 +0000

    Morada

---
+++ API/ParqueAPICentral/ParqueWEB/Models/Pagamento.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParqueAPICentral.Models
+{
+    public class Pagamento
+    {
+        public long PagamentoID { get; set; }
+        public long FaturaID { get; set; }
+    }
+}
