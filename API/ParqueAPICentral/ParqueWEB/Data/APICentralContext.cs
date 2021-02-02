2f664f0c4d7e0ca3afe670b1b27ab57aaf5eb6b2
Paulo-Torcato <paulotorcato@upskill.pt>
2021-02-02 11:06:28 +0000

    ppp

--- API/ParqueAPICentral/ParqueWEB/Data/APICentralContext.cs
+++ API/ParqueAPICentral/ParqueWEB/Data/APICentralContext.cs
@@ -15,11 +15,9 @@ namespace ParqueAPICentral.Data
         }
 
         public DbSet<ParqueAPICentral.Models.SubAluguer> SubAluguer { get; set; }
-<<<<<<< HEAD
+
         public DbSet<ParqueAPICentral.Models.Cliente> Cliente { get; set; }
 
         public DbSet<ParqueAPICentral.Models.Morada> Morada { get; set; }
-=======
->>>>>>> 645d589ea096b091927c1561540a0363b0565362
     }
 }
