fe32662b62cb815c346ec53fd20308cb2895a792
pedrocasimiro <pedro_sc90@upskill.pt>
2021-02-02 10:59:09 +0000

    "Classe Rua V2"

--- API/ParqueAPICentral/ParqueWEB/Models/Rua.cs
+++ API/ParqueAPICentral/ParqueWEB/Models/Rua.cs
@@ -7,5 +7,10 @@ namespace ParqueAPICentral.Models
 {
     public class Rua
     {
+        public long RuaID { get; set; }
+        public string NomeRua { get; set; }
+        public string CodigoPostal { get; set; }
+        public int Lotacao { get; set; } 
+
     }
 }
