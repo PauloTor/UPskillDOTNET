784a8bafc955fcd9a5614e5af0b2879bd034b80c
Paulo-Torcato <paulotorcato@upskill.pt>
2021-02-02 12:41:35 +0000

    Testar Modelo

--- API/ParqueAPICentral/ParqueWEB/Program.cs
+++ API/ParqueAPICentral/ParqueWEB/Program.cs
@@ -15,7 +15,12 @@ namespace ParqueAPICentral
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            
+            var host = CreateHostBuilder(args).Build();
+
+            CreateDbIfNotExists(host);
+
+            host.Run();
         }
 
         private static void CreateDbIfNotExists(IHost host)
