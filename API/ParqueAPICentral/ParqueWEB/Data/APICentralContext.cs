c98a29ce346f542f936ea3478e01b1c8cc9599e2
pedrocasimiro <pedro_sc90@upskill.pt>
2021-02-02 10:22:09 +0000

    "BD Ciente"

--- API/ParqueAPICentral/ParqueWEB/Data/APICentralContext.cs
+++ API/ParqueAPICentral/ParqueWEB/Data/APICentralContext.cs
@@ -15,5 +15,6 @@ namespace ParqueAPICentral.Data
         }
 
         public DbSet<ParqueAPICentral.Models.SubAluger> SubAluger { get; set; }
+        public DbSet<ParqueAPICentral.Models.Cliente> Cliente { get; set; }
     }
 }
