b0a9c8896daa428fc2656cb8dca9107bedb72293
Tiago Alves <tiagoamalves@upskill.pt>
2021-02-02 11:47:48 +0000

    Faturas Controller e Parques Controller

---
+++ API/ParqueAPICentral/ParqueWEB/Controllers/FaturasController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ParqueAPICentral.Data;
+using ParqueAPICentral.Models;
+
+namespace ParqueAPICentral.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FaturasController : ControllerBase
+    {
+        private readonly APICentralContext _context;
+
+        public FaturasController(APICentralContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Faturas
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Fatura>>> GetFatura()
+        {
+            return await _context.Fatura.ToListAsync();
+        }
+
+        // GET: api/Faturas/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Fatura>> GetFatura(long id)
+        {
+            var fatura = await _context.Fatura.FindAsync(id);
+
+            if (fatura == null)
+            {
+                return NotFound();
+            }
+
+            return fatura;
+        }
+
+        // PUT: api/Faturas/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutFatura(long id, Fatura fatura)
+        {
+            if (id != fatura.FaturaID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(fatura).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!FaturaExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Faturas
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Fatura>> PostFatura(Fatura fatura)
+        {
+            _context.Fatura.Add(fatura);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetFatura", new { id = fatura.FaturaID }, fatura);
+        }
+
+        // DELETE: api/Faturas/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteFatura(long id)
+        {
+            var fatura = await _context.Fatura.FindAsync(id);
+            if (fatura == null)
+            {
+                return NotFound();
+            }
+
+            _context.Fatura.Remove(fatura);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool FaturaExists(long id)
+        {
+            return _context.Fatura.Any(e => e.FaturaID == id);
+        }
+    }
+}
