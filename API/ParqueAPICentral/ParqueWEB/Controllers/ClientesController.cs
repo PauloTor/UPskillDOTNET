fe2b9e57e815f8a7ae9ec8ede0d4fe49748ac19d
pedrocasimiro <pedro_sc90@upskill.pt>
2021-02-04 14:53:04 +0000

    "ClientesController Put(ActualizarCliente) V1"

--- API/ParqueAPICentral/ParqueWEB/Controllers/ClientesController.cs
+++ API/ParqueAPICentral/ParqueWEB/Controllers/ClientesController.cs
@@ -4,9 +4,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using ParqueAPICentral.Models;
-using ParqueAPICentral.Data;
-using Microsoft.EntityFrameworkCore;
 
 namespace ParqueAPICentral.Controllers
 {
@@ -25,14 +22,14 @@ namespace ParqueAPICentral.Controllers
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Cliente>>> GetClientes()
         {
-            return await _context.Cliente.ToListAsync();
+            return await _context.Clientes.ToListAsync();
         }
 
         // GET: api/Clientes/5  - Obter Informação de um Cliente por ID
         [HttpGet("{id}")]
         public async Task<ActionResult<Cliente>> GetCliente(long id)
         {
-            var cliente = await _context.Cliente.FindAsync(id);
+            var cliente = await _context.Clientes.FindAsync(id);
 
             if (cliente == null)
             {
@@ -41,7 +38,35 @@ namespace ParqueAPICentral.Controllers
 
             return cliente;
         }
+        // PUT: api/Clientes/5 -  Actualizar informação de um Cliente pelo seu ID
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCliente(long id, Cliente cliente)
+        {
+            if (id != cliente.ClienteID)
+            {
+                return BadRequest();
+            }
 
+            _context.Entry(cliente).State = EntityState.Modified;
 
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ClienteExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
     }
 }
