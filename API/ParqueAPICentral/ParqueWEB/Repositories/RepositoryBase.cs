f2581a198baacc61bed3f94eb3c3621142e2622f
pedrocasimiro <pedro_sc90@upskill.pt>
2021-02-09 11:16:01 +0000

    "RepositoryBaseInitial"

--- API/ParqueAPICentral/ParqueWEB/Repositories/RepositoryBase.cs
+++ API/ParqueAPICentral/ParqueWEB/Repositories/RepositoryBase.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using ParqueAPICentral.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +9,14 @@ namespace ParqueAPICentral.Repositories
 {
     public class RepositoryBase
     {
+        protected APICentralContext RepContext { get; set; }
+
+        public RepositoryBase(APICentralContext context)
+        {
+            this.RepContext = context;
+        }
+
+
     }
+
 }
