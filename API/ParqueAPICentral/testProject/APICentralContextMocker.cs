2a9cc7445df0f64c0d7a4e2df060da328a2a965f
VicDuarte <duarte92rafc@upskill.pt>
2021-02-11 10:04:40 +0000

    Unit Testing

---
+++ API/ParqueAPICentral/testProject/APICentralContextMocker.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ParqueAPICentral.Data;
+using ParqueAPICentral.Models;
+
+namespace testProject
+{
+    class ParquePrivateAPIContextMocker
+    {
+        private static APICentralContext dbContext;
+        public static APICentralContext GetAPICentralContext(string dbName)
+        {
+            var options = new DbContextOptionsBuilder<APICentralContext>()
+                .UseInMemoryDatabase(databaseName: dbName).Options;
+            dbContext = new APICentralContext(options);
+            Seed();
+            return dbContext;
+        }
+
+        public static void Seed()
+        {       
+            dbContext.SaveChanges();
+        }
+    }
+}
