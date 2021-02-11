2424baaaf7b630928f47240a2fdbe0b3f064d162
VicDuarte <duarte92rafc@upskill.pt>
2021-02-11 16:39:01 +0000

    Unit Testing

--- API/ParqueAPICentral/testProject/APICentralContextMocker.cs
+++ API/ParqueAPICentral/testProject/APICentralContextMocker.cs
@@ -26,7 +26,9 @@ namespace testProject
             dbContext.Cliente.Add(new Cliente ("Victor Duarte", "upskill1@upskill.pt", 123456789, "PayPal", 15, 1));
             dbContext.Cliente.Add(new Cliente ("Pedro Casimiro", "upskill2@upskill.pt", 112345678, "MB", 5, 2));
             dbContext.Cliente.Add(new Cliente ("Leandro Caetano", "upskill3@upskill.pt", 122345678, "PayPal", 0, 3));
-
+            //dbContext.SubAluguer.Add(new SubAluguer(15, DateTime.Parse("2020-01-01 15:00:00"), DateTime.Parse("2020-01-05 15:00:00"), DateTime.Parse("2020-01-05 16:00:00"), 1));
+            //dbContext.SubAluguer.Add(new SubAluguer(15, DateTime.Parse("2020-02-01 15:00:00"), DateTime.Parse("2020-02-05 15:00:00"), DateTime.Parse("2020-02-05 16:00:00"), 2));
+            //dbContext.SubAluguer.Add(new SubAluguer(15, DateTime.Parse("2020-03-01 15:00:00"), DateTime.Parse("2020-03-05 15:00:00"), DateTime.Parse("2020-03-05 16:00:00"), 3));
 
             dbContext.SaveChanges();
         }
