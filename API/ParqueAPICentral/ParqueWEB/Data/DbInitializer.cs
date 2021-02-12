9d6775e4645726451998223e45ba99142c40c3fc
Leandro-Caetano <leandromccaetano@upskill.pt>
2021-02-12 22:24:07 +0000

    Parque e Morada - models e seed

--- API/ParqueAPICentral/ParqueWEB/Data/DbInitializer.cs
+++ API/ParqueAPICentral/ParqueWEB/Data/DbInitializer.cs
@@ -22,33 +22,41 @@ namespace ParqueAPICentral.Data
                 return;
             }
 
-            //IList<Parque> defaultParque = new List<Parque>();
+            var parque = new List<Parque>
+            {
+                new Parque("Aparkai 1", 1234, 10, "https://localhost:44365/api/", 1),
+                new Parque("Aparkai 2", 1234, 4, "https://localhost:44365/api/", 2),
+                new Parque("Municipal", 4444, 22, "https://localhost:44339/api/", 0)
+            };
 
-            //defaultParque.Add(new Parque("Standard 1"));
-            //defaultParque.Add(new Parque("Standard 2"));
-            //defaultParque.Add(new Parque("Standard 3"));
-            //defaultParque.Add(new Parque("Standard 4"));
-            //defaultParque.Add(new Parque("Standard 5"));
-
-            ////            _context.Fatura.Add(fatura);
-            ////          await _context.SaveChangesAsync();
-            //context.Parque.AddRange(defaultParque);
-
-            //context.SaveChanges();
+            foreach (Parque p in parque)
+            {
+                context.Parque.Add(p);
+            }
+            context.SaveChanges();
 
 
+            var morada = new List<Morada>
+            {
+                new Morada("Rua da Boavista 184", "4400-224"),
+                new Morada("Praça da Vida 83", "4428-108"),
+                new Morada("Rua do Poste", "5267-123")
+            };
+
+            foreach (Morada m in morada)
+            {
+                context.Morada.Add(m);
+            }
+            context.SaveChanges();
 
             var user = new User[]
-
-                           {
-
-
+            {
               new User { FirstName = "Test1", LastName = "User11", Username = "test1", Password = "test1" },
               new User { FirstName = "Test2", LastName = "User22", Username = "test2", Password = "test2" },
               new User { FirstName = "Test3", LastName = "User33", Username = "test3", Password = "test3" },
               new User { FirstName = "Test4", LastName = "User44", Username = "test4", Password = "test4" },
               new User { FirstName = "Test5", LastName = "User55", Username = "test5", Password = "test5" }
-                           };
+            };
 
             foreach (User s in user)
             {
@@ -58,67 +66,61 @@ namespace ParqueAPICentral.Data
 
 
 
-            var defaultCliente = new List<Cliente>();
-
-            defaultCliente.Add(new Cliente("Cliente1", "cliente1@hotmail.com", 111111111, "DD", 10,1));
-            defaultCliente.Add(new Cliente("Cliente2", "cliente2@hotmail.com", 222222222, "CartaoCredito", 20,2));
-            defaultCliente.Add(new Cliente("Cliente3", "cliente3@hotmail.com", 333333333, "CartaoCredito", 30,3));
-            defaultCliente.Add(new Cliente("Cliente4", "cliente4@hotmail.com", 444444444, "DD", 40,2));
-            defaultCliente.Add(new Cliente("Cliente6", "cliente5@hotmail.com", 555555555, "CartaoCredito", 50,1));
-            defaultCliente.Add(new Cliente("Cliente6", "cliente6@hotmail.com", 666666666, "CartaoCredito", 60,5));
+            var defaultCliente = new List<Cliente>
+            {
+                new Cliente("Cliente1", "cliente1@hotmail.com", 111111111, "DD", 10, 1),
+                new Cliente("Cliente2", "cliente2@hotmail.com", 222222222, "CartaoCredito", 20, 2),
+                new Cliente("Cliente3", "cliente3@hotmail.com", 333333333, "CartaoCredito", 30, 3),
+                new Cliente("Cliente4", "cliente4@hotmail.com", 444444444, "DD", 40, 2),
+                new Cliente("Cliente6", "cliente5@hotmail.com", 555555555, "CartaoCredito", 50, 1),
+                new Cliente("Cliente6", "cliente6@hotmail.com", 666666666, "CartaoCredito", 60, 5)
+            };
 
             context.Cliente.AddRange(defaultCliente);
 
             context.SaveChanges();
 
 
-
-
-
-            // eliminar subalugado
-
-            IList<Reserva> defaultReserva = new List<Reserva>();
-
-            defaultReserva.Add(new Reserva(11111225222, 2,3));
-            defaultReserva.Add(new Reserva(22222115111, 1,2));
-            defaultReserva.Add(new Reserva(33333341111, 4,3));
-            defaultReserva.Add(new Reserva(22222131111, 3,1));
-            defaultReserva.Add(new Reserva(22223232355, 4,5));
-            defaultReserva.Add(new Reserva(23423445645, 5,4));
+            IList<Reserva> defaultReserva = new List<Reserva>
+            {
+                new Reserva(11111225222, 2, 3),
+                new Reserva(22222115111, 1, 2),
+                new Reserva(33333341111, 4, 3),
+                new Reserva(22222131111, 3, 1),
+                new Reserva(22223232355, 4, 5),
+                new Reserva(23423445645, 5, 4)
+            };
 
             context.Reserva.AddRange(defaultReserva);
 
             context.SaveChanges();
 
 
-
-            IList<Fatura> defaultFatura = new List<Fatura>();
-
-            defaultFatura.Add(new Fatura(DateTime.Parse("2020-01-04 21:00:00"), 33, 1));
-            defaultFatura.Add(new Fatura(DateTime.Parse("2021-01-01 20:00:00"), 13, 2));
-            defaultFatura.Add(new Fatura(DateTime.Parse("2019-02-11 21:00:00"), 2, 3));
-            defaultFatura.Add(new Fatura(DateTime.Parse("2120-11-04 21:00:00"), 3, 4));
-            defaultFatura.Add(new Fatura(DateTime.Parse("2021-02-01 05:00:00"), 11, 5));
-            defaultFatura.Add(new Fatura(DateTime.Parse("2019-02-11 21:00:00"), 23, 6));
+            IList<Fatura> defaultFatura = new List<Fatura>
+            {
+                new Fatura(DateTime.Parse("2020-01-04 21:00:00"), 33, 1),
+                new Fatura(DateTime.Parse("2021-01-01 20:00:00"), 13, 2),
+                new Fatura(DateTime.Parse("2019-02-11 21:00:00"), 2, 3),
+                new Fatura(DateTime.Parse("2120-11-04 21:00:00"), 3, 4),
+                new Fatura(DateTime.Parse("2021-02-01 05:00:00"), 11, 5),
+                new Fatura(DateTime.Parse("2019-02-11 21:00:00"), 23, 6)
+            };
 
             context.Fatura.AddRange(defaultFatura);
 
             context.SaveChanges();
 
 
-            IList<SubAluguer> defaultSubAluguer = new List<SubAluguer>();
-
-            defaultSubAluguer.Add(new SubAluguer(100, DateTime.Parse("2020-01-01 17:00:00"), DateTime.Parse("2020-01-05 15:00:00"), DateTime.Parse("2020-01-05 18:00:00"), 4));
-            defaultSubAluguer.Add(new SubAluguer(15, DateTime.Parse("2020-01-04 12:00:00"), DateTime.Parse("2020-01-04 18:00:00"), DateTime.Parse("2020-01-04 20:00:00"), 3));
-            defaultSubAluguer.Add(new SubAluguer(20, DateTime.Parse("2020-01-14 14:00:00"), DateTime.Parse("2020-01-13 15:00:00"), DateTime.Parse("2020-01-14 14:00:00"), 2));
-            defaultSubAluguer.Add(new SubAluguer(15, DateTime.Parse("2020-01-25 19:00:00"), DateTime.Parse("2020-02-03 08:00:00"), DateTime.Parse("2020-02-03 19:00:00"), 1));
+            IList<SubAluguer> defaultSubAluguer = new List<SubAluguer>
+            {
+                new SubAluguer(100, DateTime.Parse("2020-01-01 17:00:00"), DateTime.Parse("2020-01-05 15:00:00"), DateTime.Parse("2020-01-05 18:00:00"), 4),
+                new SubAluguer(15, DateTime.Parse("2020-01-04 12:00:00"), DateTime.Parse("2020-01-04 18:00:00"), DateTime.Parse("2020-01-04 20:00:00"), 3),
+                new SubAluguer(20, DateTime.Parse("2020-01-14 14:00:00"), DateTime.Parse("2020-01-13 15:00:00"), DateTime.Parse("2020-01-14 14:00:00"), 2),
+                new SubAluguer(15, DateTime.Parse("2020-01-25 19:00:00"), DateTime.Parse("2020-02-03 08:00:00"), DateTime.Parse("2020-02-03 19:00:00"), 1)
+            };
 
             context.SubAluguer.AddRange(defaultSubAluguer);
             context.SaveChanges();
-
-
-
-
         }
     }
 }
--- API/ParqueAPICentral/ParqueWEB/Models/Parque.cs
+++ API/ParqueAPICentral/ParqueWEB/Models/Parque.cs
@@ -4,6 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
+
 namespace ParqueAPICentral.Models
 
 {
@@ -14,20 +15,25 @@ namespace ParqueAPICentral.Models
 
         public string NomeParque { get; set; }
 
-        public int NIFParque { get; set; }
+        public long NIFParque { get; set; }
 
-        public int Lotacao { get; set; }
+        public int Lotacao { get; set; }      
+        
+        [Url]
+        public string Url { get; set; }
+
         [ForeignKey("MoradaID")]
         public long MoradaID { get; set; }
-
-        public string Url { get; set; }
-        public Morada morada {get;set;}
+        public Morada Morada {get;set;}
         
-        public Parque(string nomeParque)
+        public Parque(string nomeParque, long nifParque, int lotacao, string url, long moradaId)
         {
+            NomeParque = nomeParque;
+            NIFParque = nifParque;
+            Lotacao = lotacao;
+            Url = url;
+            MoradaID = moradaId;
         }
-
-
     }
 }
  
