68737e06d1b16f6bfb06074297182287dda94390
Tiago Alves <tiagoamalves@upskill.pt>
2021-02-12 09:43:09 +0000

    PostFaturaByReservaID e GetFaturaByFaturaID sem services ou repos

--- API/ParqueAPICentral/ParqueWEB/Controllers/FaturasController.cs
+++ API/ParqueAPICentral/ParqueWEB/Controllers/FaturasController.cs
@@ -10,6 +10,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json;
 using ParqueAPICentral.Data;
+using ParqueAPICentral.DTO;
 using ParqueAPICentral.Entities;
 using ParqueAPICentral.Models;
 using ParqueAPICentral.Services;
@@ -20,43 +21,134 @@ namespace ParqueAPICentral.Controllers
     [ApiController]
     public class FaturasController : ControllerBase
     {
-        private readonly FaturaService _service;
-        private readonly IConfiguration _configure;
-        private readonly string apiBaseUrl;
-        private readonly string apiBaseUrl2;
+        private readonly APICentralContext _context;
 
+        //private readonly FaturaService _service;
+        //private readonly IConfiguration _configure;
+        //private readonly string apiBaseUrl;
+        //private readonly string apiBaseUrl2;
 
-        public FaturasController(FaturaService service, IConfiguration configuration)
+        public FaturasController(APICentralContext context)
         {
-            this._service = service;
-            _configure = configuration;
-            apiBaseUrl2 = _configure.GetValue<string>("WebAPICentralBaseUrl");
-            apiBaseUrl = _configure.GetValue<string>("WebAPIPrivateBaseUrl");
+            _context = context;
         }
 
-        // POST Faturas by ReservaID - api/Faturas/Reserva/ReservaID
+
+
+        //public FaturasController(FaturaService service, IConfiguration configuration)
+        //{
+        //    this._service = service;
+        //    _configure = configuration;
+        //    apiBaseUrl2 = _configure.GetValue<string>("WebAPICentralBaseUrl");
+        //    apiBaseUrl = _configure.GetValue<string>("WebAPIPrivateBaseUrl");
+        //}
+
+        //// POST Faturas by ReservaID - api/Faturas/Reserva/ReservaID
+        //[EnableCors]
+        //[HttpPost("Reserva/{ReservaID}")]
+        //public async Task<ActionResult<Fatura>> PostFaturaByReservaID(long reservaID)
+        //{
+        //    return await this._service.CreateFaturaByReservaID(reservaID);
+        //}
+
+        //// GET Faturas by FaturaID - api/Faturas/5
+        //[HttpGet("{FaturaID}")]
+        //public IActionResult GetFatura(long FaturaID)
+        //{
+        //    var Fatura = this._service.FindFaturaByID(FaturaID);
+
+        //    if (Fatura == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    return Ok(Fatura);
+
+        //}
+
+        // POST Faturas by ReservaID - api/Faturas/ReservaID - sem Services e Repositories
         [EnableCors]
-        [HttpPost("Reserva/{ReservaID}")]
-        public async Task<ActionResult<Fatura>> PostFaturaByReservaID(long reservaID)
+        [HttpPost("{ReservaID}")]
+        public async Task<ActionResult<IEnumerable<Fatura>>> PostFaturaByReserva(long ReservaID)
         {
-            return await this._service.CreateFaturaByReservaID(reservaID);
+            var reserva = await _context.Reserva.FindAsync(ReservaID);
+            string BaseUrl = "https://localhost:44365/";
+            string BaseUrl2 = "https://localhost:44330/";
+            var ListaReservas = new List<Reserva_>();
+            var ListaLugares = new List<Lugar_>();
+            Fatura fatura;
+            using (var client = new HttpClient())
+
+            {
+
+                var pesquisaReserva = reserva.ReservaID;
+                client.BaseAddress = new Uri(BaseUrl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                // Route para Reservas
+                string endpoint = BaseUrl + "api/Reservas/" + pesquisaReserva;
+                var response = await client.GetAsync(endpoint);
+                response.EnsureSuccessStatusCode();
+
+                var _Reserva = await response.Content.ReadAsAsync<Reserva_>();
+
+                var reservaIdent = _Reserva.ReservaID;
+
+                var _lugar = _Reserva.LugarID;
+                var _dataInicio = _Reserva.DataInicio;
+                var _dataFim = _Reserva.DataFim;
+
+                // Route para LugarID (_lugar)
+                string endpoint2 = BaseUrl + "api/Lugares/" + _lugar;
+                var response2 = await client.GetAsync(endpoint2);
+                response2.EnsureSuccessStatusCode();
+                var _Lugar = await response2.Content.ReadAsAsync<Lugar_>();
+
+                //Preco por hora
+                var _preco = _Lugar.Preço;
+
+                //Conversão para horas
+                var timeSpan = _dataFim.Subtract(_dataInicio);
+                var horas = timeSpan.Hours;
+
+                //Preço por hora * quantidade de horas
+                var PrecoFatura = horas * _preco;
+
+                var dataFatura = DateTime.Now;
+
+                //cria instancia para uma nova fatura
+                fatura = new Fatura(dataFatura, PrecoFatura, reservaIdent);
+                //Passa a fatura para formato JSON
+                StringContent fatura_ = new StringContent(JsonConvert.SerializeObject(fatura), Encoding.UTF8, "application/json");
+                string endpoint3 = BaseUrl2 + "API/Faturas/";
+                //POST Fatura
+                var response3 = await client.PostAsync(endpoint3, fatura_);
+
+                _context.Fatura.Add(fatura);
+                await _context.SaveChangesAsync();
+
+                //return CreatedAtAction("GetFatura", new { id = fatura.FaturaID }, fatura);
+
+                return NoContent();
+            }
+
         }
 
-        // GET Faturas by FaturaID - api/Faturas/5
+        // GET Faturas by FaturaID - api/Faturas/5 - sem services ou repositories
         [HttpGet("{FaturaID}")]
-        public IActionResult GetFatura(long FaturaID)
+        public async Task<ActionResult<Fatura>> GetFatura(long FaturaID)
         {
-            var Fatura = this._service.FindFaturaByID(FaturaID);
+            var fatura = await _context.Fatura.FindAsync(FaturaID);
 
-            if (Fatura == null)
+            if (fatura == null)
             {
                 return NotFound();
             }
 
-            return Ok(Fatura);
-
+            return fatura;
         }
 
     }
 }
+
 
