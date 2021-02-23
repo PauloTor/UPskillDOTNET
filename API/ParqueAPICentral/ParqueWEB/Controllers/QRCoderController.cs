06bfb3df263f7377ea3c07085c5001ee119daea3
Leandro-Caetano <leandromccaetano@upskill.pt>
2021-02-23 10:30:34 +0000

    qrCoder

---
+++ API/ParqueAPICentral/ParqueWEB/Controllers/QRCoderController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Drawing;
+using System.IO;
+using QRCoder;
+using ParqueAPICentral.DTO;
+using ParqueAPICentral.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ParqueAPICentral.Controllers
+{
+    public class QRCoderController : ControllerBase
+    {
+
+        public ActionResult<byte[]> QRcoder(Reserva_ reserva_, Reserva reserva)
+        {
+            var qrInfo = ("Parque: " + reserva.Parque.NomeParque
+                + "\n Morada: " + reserva.Parque.Morada.Rua
+                + "\n Lugar: " + reserva_.LugarID
+                + "\n Data de Inicio: " + reserva_.DataInicio
+                + "\n Data de Fim: " + reserva_.DataFim);
+
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(qrInfo, QRCodeGenerator.ECCLevel.Q);
+
+            QRCode qrCode = new QRCode(qrCodeData);
+
+            Bitmap qrCodeImage = qrCode.GetGraphic(20);
+
+            return BitmapToBytes(qrCodeImage);
+        }
+
+
+        private static byte[] BitmapToBytes(Bitmap img)
+        {
+            using MemoryStream stream = new MemoryStream();
+
+            img.Save(stream, System.Drawing.Imaging.ImageFormat.Png);
+
+            return stream.ToArray();
+        }
+    }
+}
---
+++ API/ParqueAPICentral/ParqueWEB/Migrations/20210217091935_2.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ParqueAPICentral.Migrations
+{
+    public partial class _2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "NovoCliente",
+                table: "SubAluguer",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Reservado",
+                table: "SubAluguer",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NovoCliente",
+                table: "SubAluguer");
+
+            migrationBuilder.DropColumn(
+                name: "Reservado",
+                table: "SubAluguer");
+        }
+    }
+}
