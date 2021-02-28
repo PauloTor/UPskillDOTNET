a1c3840bcb368af8cc5577cfc81c81187744dd6a
pedrocasimiro <pedro_sc90@upskill.pt>
2021-02-28 15:29:21 +0000

    "ClientesDetails"

--- API/PseudoCompanyFront/PseudoCompanyFront/Controllers/ClientesController.cs
+++ API/PseudoCompanyFront/PseudoCompanyFront/Controllers/ClientesController.cs
@@ -1,45 +1,64 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Net.Http;
-//using System.Threading.Tasks;
-//using Microsoft.AspNetCore.Authorization;
-//using Microsoft.AspNetCore.Mvc;
-//using Microsoft.AspNetCore.Mvc.Rendering;
-//using Microsoft.EntityFrameworkCore;
-//using Microsoft.Extensions.Configuration;
-//using PseudoCompanyFront.Data;
-//using PseudoCompanyFront.Models;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using PseudoCompanyFront.Data;
+using PseudoCompanyFront.Models;
 
-//namespace PseudoCompanyFront.Controllers
-//{
-//    public class ClientesController : Controller
-//    {
-//        private readonly IConfiguration _configure;
-//        private readonly string apiBaseUrl;
+namespace PseudoCompanyFront.Controllers
+{
+    public class ClientesController : Controller
+    {
+        private readonly IConfiguration _configure;
+        private readonly string apiBaseUrl;
 
-//        // Construtor do controller
-//        public ClientesController(IConfiguration configuration)
-//        {
-//            _configure = configuration;
-//            apiBaseUrl = _configure.GetValue<string>("WebAPIBaseUrl");
-//        }
-//        // GET: api/Clientes : Obter Informação dos Clientes
-//        [Authorize(Policy = "Admin")]
-//        [HttpGet]
-//        public async Task<ActionResult<IEnumerable<Cliente>>> Get_Clientes()
-//        {
-//            using (HttpClient client = new HttpClient())
-//            {
-//                string endpoint = apiBaseUrl + "/Clientes";
-//                var response = await client.GetAsync(endpoint);
-//                response.EnsureSuccessStatusCode();
-//                Clientes = await response.Content.ReadAsAsync<Cliente>();
-//                return Clientes;
-//            }
+        // Construtor do controller
+        public ClientesController(IConfiguration configuration)
+        {
+            _configure = configuration;
+            apiBaseUrl = _configure.GetValue<string>("WebAPIBaseUrl");
+        }
+        // GET: Obter informação de todos os Clientes
+        public async Task<IActionResult> Index(string searchString)
+        {
+            ViewData["CurrentFilter"] = searchString;
+            var listaClientes = new List<Cliente>();
+            using (HttpClient client = new HttpClient())
+            {
+                string endpoint = apiBaseUrl + "/Clientes";
+                var response = await client.GetAsync(endpoint);
+                response.EnsureSuccessStatusCode();
+                listaClientes = await response.Content.ReadAsAsync<List<Cliente>>();
+            }
+            return View(listaClientes);
+        }
+        // GET: Clientes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            Cliente cliente;
+            using (HttpClient client = new HttpClient())
+            {
+                string endpoint = apiBaseUrl + "/cliente/" + id;
+                var response = await client.GetAsync(endpoint);
+                response.EnsureSuccessStatusCode();
+                cliente = await response.Content.ReadAsAsync<Cliente>();
+            }
+            if (cliente == null)
+            {
+                return NotFound();
+            }
+            return View(cliente);
+        }
+    }
+}
 
-
-//        }
-
-//    }
-//}
