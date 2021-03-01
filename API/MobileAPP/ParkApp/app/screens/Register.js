7fd259649ec727d07fa16c9759994d6d6efcebf4
VicDuarte <duarte92rafc@upskill.pt>
2021-03-01 18:46:49 +0000

    Mobile app

---
+++ API/MobileAPP/ParkApp/app/screens/Register.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components/native';
+
+const Container = styled.View``;
+const Text = styled.Text``;
+
+function Login() {
+    return (
+        <Container>
+            <Text>Login</Text>
+        </Container>
+    );
+}
+
+export default Login;
