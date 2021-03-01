0b1b622c71bc32c8f8d6f247f9acff439b19b41f
pedrocasimiro <pedro_sc90@upskill.pt>
2021-03-01 11:14:47 +0000

    "POST Clientes/Create"

--- API/PseudoCompanyFront/PseudoCompanyFront/Controllers/ClientesController.cs
+++ API/PseudoCompanyFront/PseudoCompanyFront/Controllers/ClientesController.cs
@@ -76,33 +76,26 @@ namespace PseudoCompanyFront.Controllers
             }
             return View();
         }
+
+        //POST Clientes/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        //[Authorize(Roles = "Funcionario,Administrador")]
+
+        public async Task<IActionResult> Create([Bind("ClienteID,NomeCliente,EmailCliente,NifCliente,MetodoPagamento,Credito")] Cliente cliente)
+        {
+            if (ModelState.IsValid)
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    StringContent content = new StringContent(JsonConvert.SerializeObject(cliente), Encoding.UTF8, "application/json");
+                    string endpoint = apiBaseUrl + "/Cliente";
+                    var response = await client.PostAsync(endpoint, content);
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(cliente);
+        }
     }
 }
-
-        //CREATE POST
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        ////[Authorize(Roles = "Funcionario,Administrador")]
-
-        //public async Task<IActionResult> Create([Bind("ClienteID,NomeCliente,EmailCliente,NifCliente,MetodoPagamento,Credito")] Cliente cliente)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        using (HttpClient client = new HttpClient())
-        //        {
-        //            //UserInfo user = new UserInfo();
-        //            //StringContent contentUser = new StringContent(JsonConvert.SerializeObject(user), Encoding.UTF8, "application/json");
-        //            //var responseLogin = await client.PostAsync(apiBaseUrl + "/users/login", contentUser);
-        //            //UserToken token = await responseLogin.Content.ReadAsAsync<UserToken>();
-        //            //client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token.Token);
-        //            StringContent content = new StringContent(JsonConvert.SerializeObject(cliente), Encoding.UTF8, "application/json");
-        //            string endpoint = apiBaseUrl + "/Cliente";
-        //            var response = await client.PostAsync(endpoint, content);
-        //        }
-        //        return RedirectToAction(nameof(Index));
-        //    }
-        //    return View(cliente);
-//        }
-//    }
-//}
 
