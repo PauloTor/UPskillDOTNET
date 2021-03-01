0b0ff9d47d833d62374c43f9c08e055743d12884
pedrocasimiro <pedro_sc90@upskill.pt>
2021-03-01 09:42:57 +0000

    "Layout"

--- API/PseudoCompanyFront/PseudoCompanyFront/Controllers/ClientesController.cs
+++ API/PseudoCompanyFront/PseudoCompanyFront/Controllers/ClientesController.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
 using PseudoCompanyFront.Data;
 using PseudoCompanyFront.Models;
 
@@ -39,7 +41,7 @@ namespace PseudoCompanyFront.Controllers
             return View(listaClientes);
         }
         // GET: Clientes/Details/5
-        public async Task<IActionResult> Details(int? id)
+        public async Task<IActionResult> Details(long? id)
         {
             if (id == null)
             {
@@ -48,7 +50,7 @@ namespace PseudoCompanyFront.Controllers
             Cliente cliente;
             using (HttpClient client = new HttpClient())
             {
-                string endpoint = apiBaseUrl + "/cliente/" + id;
+                string endpoint = apiBaseUrl + "/Clientes/" + id;
                 var response = await client.GetAsync(endpoint);
                 response.EnsureSuccessStatusCode();
                 cliente = await response.Content.ReadAsAsync<Cliente>();
@@ -56,6 +58,51 @@ namespace PseudoCompanyFront.Controllers
             if (cliente == null)
             {
                 return NotFound();
+            }
+            return View(cliente);
+        }
+
+        // CREATE GET
+
+        //public async Task<IActionResult> Create()
+        //{
+        //    var listaVirus = new List<Virus>();
+        //    using (HttpClient client = new HttpClient())
+        //    {
+        //        UserInfo user = new UserInfo();
+        //        StringContent contentUser = new StringContent(JsonConvert.SerializeObject(user), Encoding.UTF8, "application/json");
+        //        var responseLogin = await client.PostAsync(apiBaseUrl + "/users/login", contentUser);
+        //        UserToken token = await responseLogin.Content.ReadAsAsync<UserToken>();
+        //        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token.Token);
+        //        string endpoint = apiBaseUrl + "/virus";
+        //        var response = await client.GetAsync(endpoint);
+        //        response.EnsureSuccessStatusCode();
+        //        listaVirus = await response.Content.ReadAsAsync<List<Virus>>();
+        //    }
+        //    return View();
+        //}
+
+        //CREATE POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        //[Authorize(Roles = "Funcionario,Administrador")]
+
+        public async Task<IActionResult> Create([Bind("ClienteID,NomeCliente,EmailCliente,NifCliente,MetodoPagamento,Credito")] Cliente cliente)
+        {
+            if (ModelState.IsValid)
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    //UserInfo user = new UserInfo();
+                    //StringContent contentUser = new StringContent(JsonConvert.SerializeObject(user), Encoding.UTF8, "application/json");
+                    //var responseLogin = await client.PostAsync(apiBaseUrl + "/users/login", contentUser);
+                    //UserToken token = await responseLogin.Content.ReadAsAsync<UserToken>();
+                    //client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token.Token);
+                    StringContent content = new StringContent(JsonConvert.SerializeObject(cliente), Encoding.UTF8, "application/json");
+                    string endpoint = apiBaseUrl + "/Cliente";
+                    var response = await client.PostAsync(endpoint, content);
+                }
+                return RedirectToAction(nameof(Index));
             }
             return View(cliente);
         }
