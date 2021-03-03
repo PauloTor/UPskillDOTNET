5b896d729434c4b4fec2eba6a5166f6c0f1455b7
Leandro-Caetano <leandromccaetano@upskill.pt>
2021-03-03 11:50:26 +0000

    view reserva

--- API/PseudoCompanyFront/PseudoCompanyFront/Models/Reserva.cs
+++ API/PseudoCompanyFront/PseudoCompanyFront/Models/Reserva.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,12 +8,16 @@ namespace PseudoCompanyFront.Models
 {
     public class Reserva
     {
+        [Display(Name = "Nº Reserva")]
         public long ReservaID { get; set; }
 
+        [Display(Name = "Nº Parque")]
         public long ParqueID { get; set; }
 
+        [Display(Name = "Nº Lugar")]
         public long LugarID { get; set; }
 
+        [Display(Name = "Nº Cliente")]
         public long ClienteID { get; set; }
 
         public Reserva Reservas { get; }
