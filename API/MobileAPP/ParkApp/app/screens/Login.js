10eec5630d3c183bee04634c8a7a69efb13fb0f6
VicDuarte <duarte92rafc@upskill.pt>
2021-03-04 16:46:18 +0000

    update MobileApp

--- API/MobileAPP/ParkApp/app/screens/Login.js
+++ API/MobileAPP/ParkApp/app/screens/Login.js
@@ -1,17 +1,66 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styled from 'styled-components/native';
 
-const Container = styled.View``;
-const Text = styled.Text``;
-const Button = styled.Button``;
+import TextInput from '../components/TextInput';
+import AuthContext from '../AuthContext';
+
+const Container = styled.View`
+  flex: 1;
+  background: #fdfffc;
+  justify-content: center;
+`;
+const Title = styled.Text`
+  font-size: 36px;
+  color: #011627;
+  padding: 4px 16px;
+`;
+const Content = styled.View`
+  flex: 1;
+  padding: 20% 16px 0 16px;
+`;
+
+const TochableOpacity = styled.TouchableOpacity`
+  background: #2ec4b6;
+  align-items: center;
+  justify-content: center;
+  margin: 16px 0;
+  height: 40px;
+  border-radius: 3px;
+`;
+
+const ButtonText = styled.Text`
+  font-size: 16px;
+  color: #fdfffc;
+`;
+
+const Label = styled.Text`
+  font-size: 16px;
+  margin: 8px 0 4px 0;
+`;
 
 function Login({navigation}) {
-    return (
-        <Container>
-            <Text>Login</Text>
-            <Button title="Register" onPress={() => navigation.navigate('Register')} />
-        </Container>
-    );
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+ // const {signIn} = React.useContext(AuthContext);
+ const signIn = (username, password) => {
+   console.log(username + ":" + password)
+ }
+
+  return (
+    <Container>
+      <Title>Login</Title>
+      <Content>
+        <Label>Username</Label>
+        <TextInput placeholder="username" value={username} onChangeText={(text) => {setUsername(text);}} />
+        <Label>Password</Label>
+        <TextInput placeholder="password" secureTextEntry={true} value={password} onChangeText={(text) => {setPassword(text);}} />
+        <TochableOpacity onPress={() => signIn(username, password)}>
+          <ButtonText>LOGIN</ButtonText>
+        </TochableOpacity>
+      </Content>
+    </Container>
+  );
 }
 
 export default Login;
