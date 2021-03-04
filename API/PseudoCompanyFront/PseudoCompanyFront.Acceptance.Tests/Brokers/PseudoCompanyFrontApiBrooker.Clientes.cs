5f84e4f614794bbec8b578291640ad442d579e60
Tiago Alves <tiagoamalves@upskill.pt>
2021-03-04 09:10:53 +0000

    E2E testing

---
+++ API/PseudoCompanyFront/PseudoCompanyFront.Acceptance.Tests/Brokers/PseudoCompanyFrontApiBrooker.Clientes.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PseudoCompanyFront.Acceptance.Tests.Brokers
+{
+    public partial class PseudoCompanyFrontApiBrooker
+    {
+    }
+}
