81570550313b1d603d1938927e282fb0e1b48d4f
pedrocasimiro <pedro_sc90@upskill.pt>
2021-03-08 09:57:38 +0000

    "PaginatedList"

--- API/PseudoCompany__/PseudoCompany__/Helpers/AuthorizeAttribute.cs
+++ API/PseudoCompany__/PseudoCompany__/Helpers/AuthorizeAttribute.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Azure.Documents;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
