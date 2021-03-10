70f6db36ef915ec396006547880bf39cce9c59de
Tiago Alves <tiagoamalves@upskill.pt>
2021-03-10 10:15:12 +0000

    Homepage, LoginPage e LoginTest

---
+++ API/SeleniumTests/SeleniumTests/Pages/LoginPage.cs
@@ -0,0 +1,36 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumTests.Pages
+{
+    class LoginPage
+    {
+
+        public LoginPage (IWebDriver driver)
+        {
+            Driver = driver;
+        }
+
+        public IWebDriver Driver { get; }
+
+        IWebElement txtUserName => Driver.FindElement(By.Name("UserName"));
+
+        IWebElement txtPassword => Driver.FindElement(By.Name("Password"));
+
+        IWebElement btnLogin => Driver.FindElement(By.XPath("/html/body/div[1]/main/div/div/form/div[3]/input"));
+
+
+        public void Login(string userName, string password)
+        {
+
+            txtUserName.SendKeys(userName);
+            txtPassword.SendKeys(password);
+            btnLogin.Submit();
+        }
+
+    }
+}
